fc2712549157c7c7754577e1ba6a79dd5125f889
Ardee <goobama@outlook.com>
2016-05-27 20:25:59 +0800

    Initial Commit

---
+++ VideoRanker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoRanker
+//
+//  Created by Ardee on 16/5/27.
+//  Copyright © 2016年 Ardee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoRanker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VideoRanker
+//
+//  Created by Ardee on 16/5/27.
+//  Copyright © 2016年 Ardee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
